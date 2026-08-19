de2f3c00da8b7385bbc11aca6104f3288dd2445e
empereurstephane3-cell <empereurstephane3@gmail.com>
2026-08-19 22:39:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -73,8 +73,8 @@ fixtures = get_fixtures(date_str)
 if not fixtures and API_KEY == "65ad65cff78e2148482946179f1a89300f749a0ae3b6d8db848ffbc41901dc4e":
      # st.warning(
         "⚠️ Pense à insérer ta clé API-Football dans le code pour récupérer"
-        " les vrais matchs en direct !"
-    )
+        " les vrais matchs en direct !")
+    
     # Données fictives de secours pour tester l'affichage si pas de clé
     fixtures = [
         {
